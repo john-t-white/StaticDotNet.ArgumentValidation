8dafc72e9b0f87bea22bdfb1f2df77e08e27fc09
John White <johnwhite@kaplantrucking.com>
2022-11-22 08:23:08 -0500

    Split benchmarks for null check for both class and struct

--- StaticDotNet.ArgumentValidation.Benchmarks/NotNullStructBenchmarks.cs
+++ StaticDotNet.ArgumentValidation.Benchmarks/NotNullStructBenchmarks.cs
@@ -5,27 +5,27 @@ namespace StaticDotNet.ArgumentValidation.Benchmarks;
 
 [MemoryDiagnoser]
 [SimpleJob( RuntimeMoniker.Net70 )]
-public class NotNullBenchmarks {
+public class NotNullStructBenchmarks {
 
-	public object value = new();
+	public int? value = new();
 
 	[Benchmark( Baseline = true )]
-	public object Baseline() {
+	public int Baseline() {
 		return this.value ?? throw new ArgumentNullException( nameof( this.value ) );
 	}
 
 	[Benchmark]
-	public object Argument_Is_NotNull() {
+	public int Argument_Is_NotNull() {
 		return Argument.Is.NotNull( this.value );
 	}
 
 	[Benchmark]
-	public object Dawn_Guard_NotNull() {
+	public int Dawn_Guard_NotNull() {
 		return Dawn.Guard.Argument( this.value ).NotNull();
 	}
 
 	[Benchmark]
-	public object Ardalis_Guard_NotNull() {
-		return Ardalis.GuardClauses.Guard.Against.Null( this.value );
+	public int Ardalis_Guard_NotNull() {
+		return Ardalis.GuardClauses.Guard.Against.Null( this.value ).Value;
 	}
 }
--- StaticDotNet.ArgumentValidation.Benchmarks/Program.cs
+++ StaticDotNet.ArgumentValidation.Benchmarks/Program.cs
@@ -1,8 +1,9 @@
 ﻿using BenchmarkDotNet.Running;
 using StaticDotNet.ArgumentValidation.Benchmarks;
 
-//_ = BenchmarkRunner.Run<NotNullBenchmarks>();
+//_ = BenchmarkRunner.Run<NotNullClassBenchmarks>();
+_ = BenchmarkRunner.Run<NotNullStructBenchmarks>();
 //_ = BenchmarkRunner.Run<NullBenchmarks>();
 //_ = BenchmarkRunner.Run<TrueBenchmarks>();
 //_ = BenchmarkRunner.Run<NullableTrueBenchmarks>();
-_ = BenchmarkRunner.Run<NotNullTrueBenchmarks>();
+//_ = BenchmarkRunner.Run<NotNullTrueBenchmarks>();
