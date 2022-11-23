e15d48194a99c63a7f471866c2ac1fbd256ff5cc
John White <johnwhite@kaplantrucking.com>
2022-11-23 15:04:18 -0500

    Added intellisense to GreaterThan checks.

--- StaticDotNet.ArgumentValidation/RangeExtensions.GreaterThan.cs
+++ StaticDotNet.ArgumentValidation/RangeExtensions.GreaterThan.cs
@@ -7,32 +7,108 @@ using System.Threading.Tasks;
 
 namespace StaticDotNet.ArgumentValidation;
 
+/// <summary>
+/// Validation methods for checking value ranges.
+/// </summary>
 public static partial class RangeExtensions {
 
+	/// <summary>
+	/// Validates <paramref name="value"/> is not null and greater than <paramref name="comparisonValue"/>, otherwise an <see cref="ArgumentNullException"/> or <see cref="ArgumentException"/> is thrown.
+	/// </summary>
+	/// <typeparam name="T">The type of <paramref name="value"/>.</typeparam>
+	/// <param name="argument">The <see cref="Argument"/>.</param>
+	/// <param name="value">The value of the argument.</param>
+	/// <param name="comparisonValue">The value to compare against.</param>
+	/// <param name="name">With C# 10, defaults to the expression of <paramref name="value"/>; otherwise specify the argument name.</param>
+	/// <param name="message">The exception message.  Null for for default message.</param>
+	/// <returns>Returns <paramref name="value"/>.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+	/// <exception cref="ArgumentException">Thrown when <paramref name="value"/> is not greater than <paramref name="comparisonValue"/>.</exception>
 	[return: NotNull]
 	public static T NotNullGreaterThan<T>( this Argument argument, [NotNull] T? value, T comparisonValue, [CallerArgumentExpression( nameof( value ) )] string? name = null, string? message = null )
 		where T : class
 		=> argument.GreaterThan( argument.NotNull( value, name, message ), comparisonValue, Comparer<T>.Default, name, message );
 
+	/// <summary>
+	/// Validates <paramref name="value"/> is not null and greater than <paramref name="comparisonValue"/>, otherwise an <see cref="ArgumentNullException"/> or <see cref="ArgumentException"/> is thrown.
+	/// </summary>
+	/// <typeparam name="T">The type of <paramref name="value"/>.</typeparam>
+	/// <param name="argument">The <see cref="Argument"/>.</param>
+	/// <param name="value">The value of the argument.</param>
+	/// <param name="comparisonValue">The value to compare against.</param>
+	/// <param name="comparer">The <see cref="IComparer{T}"/> to use.</param>
+	/// <param name="name">With C# 10, defaults to the expression of <paramref name="value"/>; otherwise specify the argument name.</param>
+	/// <param name="message">The exception message.  Null for for default message.</param>
+	/// <returns>Returns <paramref name="value"/>.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+	/// <exception cref="ArgumentException">Thrown when <paramref name="value"/> is not greater than <paramref name="comparisonValue"/>.</exception>
 	[return: NotNull]
 	public static T NotNullGreaterThan<T>( this Argument argument, [NotNull] T? value, T comparisonValue, IComparer<T> comparer, [CallerArgumentExpression( nameof( value ) )] string? name = null, string? message = null )
 		where T : class
 		=> argument.GreaterThan( argument.NotNull( value, name, message ), comparisonValue, comparer, name, message );
 
+	/// <summary>
+	/// Validates <paramref name="value"/> is not null and greater than <paramref name="comparisonValue"/>, otherwise an <see cref="ArgumentNullException"/> or <see cref="ArgumentException"/> is thrown.
+	/// </summary>
+	/// <typeparam name="T">The type of <paramref name="value"/>.</typeparam>
+	/// <param name="argument">The <see cref="Argument"/>.</param>
+	/// <param name="value">The value of the argument.</param>
+	/// <param name="comparisonValue">The value to compare against.</param>
+	/// <param name="name">With C# 10, defaults to the expression of <paramref name="value"/>; otherwise specify the argument name.</param>
+	/// <param name="message">The exception message.  Null for for default message.</param>
+	/// <returns>Returns <paramref name="value"/>.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+	/// <exception cref="ArgumentException">Thrown when <paramref name="value"/> is not greater than <paramref name="comparisonValue"/>.</exception>
 	[return: NotNull]
 	public static T NotNullGreaterThan<T>( this Argument argument, [NotNull] T? value, T comparisonValue, [CallerArgumentExpression( nameof( value ) )] string? name = null, string? message = null )
 		where T : struct
 		=> argument.GreaterThan( argument.NotNull( value, name, message ), comparisonValue, Comparer<T>.Default, name, message );
 
+	/// <summary>
+	/// Validates <paramref name="value"/> is not null and greater than <paramref name="comparisonValue"/>, otherwise an <see cref="ArgumentNullException"/> or <see cref="ArgumentException"/> is thrown.
+	/// </summary>
+	/// <typeparam name="T">The type of <paramref name="value"/>.</typeparam>
+	/// <param name="argument">The <see cref="Argument"/>.</param>
+	/// <param name="value">The value of the argument.</param>
+	/// <param name="comparisonValue">The value to compare against.</param>
+	/// <param name="comparer">The <see cref="IComparer{T}"/> to use.</param>
+	/// <param name="name">With C# 10, defaults to the expression of <paramref name="value"/>; otherwise specify the argument name.</param>
+	/// <param name="message">The exception message.  Null for for default message.</param>
+	/// <returns>Returns <paramref name="value"/>.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+	/// <exception cref="ArgumentException">Thrown when <paramref name="value"/> is not greater than <paramref name="comparisonValue"/>.</exception>
 	[return: NotNull]
 	public static T NotNullGreaterThan<T>( this Argument argument, [NotNull] T? value, T comparisonValue, IComparer<T> comparer, [CallerArgumentExpression( nameof( value ) )] string? name = null, string? message = null )
 		where T : struct
 		=> argument.GreaterThan( argument.NotNull( value, name, message ), comparisonValue, comparer, name, message );
 
+	/// <summary>
+	/// Validates <paramref name="value"/> is greater than <paramref name="comparisonValue"/>, otherwise an <see cref="ArgumentException"/> is thrown.
+	/// </summary>
+	/// <typeparam name="T">The type of <paramref name="value"/>.</typeparam>
+	/// <param name="argument">The <see cref="Argument"/>.</param>
+	/// <param name="value">The value of the argument.</param>
+	/// <param name="comparisonValue">The value to compare against.</param>
+	/// <param name="name">With C# 10, defaults to the expression of <paramref name="value"/>; otherwise specify the argument name.</param>
+	/// <param name="message">The exception message.  Null for for default message.</param>
+	/// <returns>Returns <paramref name="value"/>.</returns>
+	/// <exception cref="ArgumentException">Thrown when <paramref name="value"/> is not greater than <paramref name="comparisonValue"/>.</exception>
 	[return: NotNullIfNotNull( nameof( value ) )]
 	public static T? GreaterThan<T>( this Argument argument, T? value, T comparisonValue, [CallerArgumentExpression( nameof( value ) )] string? name = null, string? message = null )
 		=> argument.GreaterThan( value, comparisonValue, Comparer<T>.Default, name, message );
 
+	/// <summary>
+	/// Validates <paramref name="value"/> is greater than <paramref name="comparisonValue"/>, otherwise an <see cref="ArgumentException"/> is thrown.
+	/// </summary>
+	/// <typeparam name="T">The type of <paramref name="value"/>.</typeparam>
+	/// <param name="_">The <see cref="Argument"/>.</param>
+	/// <param name="value">The value of the argument.</param>
+	/// <param name="comparisonValue">The value to compare against.</param>
+	/// <param name="comparer">The <see cref="IComparer{T}"/> to use.</param>
+	/// <param name="name">With C# 10, defaults to the expression of <paramref name="value"/>; otherwise specify the argument name.</param>
+	/// <param name="message">The exception message.  Null for for default message.</param>
+	/// <returns>Returns <paramref name="value"/>.</returns>
+	/// <exception cref="ArgumentException">Thrown when <paramref name="value"/> is not greater than <paramref name="comparisonValue"/>.</exception>
 	[return: NotNullIfNotNull( nameof( value ) )]
 	public static T? GreaterThan<T>( this Argument _, T? value, T comparisonValue, IComparer<T> comparer, [CallerArgumentExpression( nameof( value ) )] string? name = null, string? message = null )
 		=> value == null
