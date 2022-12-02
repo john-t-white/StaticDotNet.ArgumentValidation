3be132e07072436ba9756a7640f25c1872ebc986
John White <johnwhite@kaplantrucking.com>
2022-12-02 15:58:18 -0500

    Added unit tests for different types of enumerables.

---
+++ src/StaticDotNet.ArgumentValidation.UnitTests/EnumerableExtensions_NotEmpty_Array.cs
@@ -0,0 +1,82 @@
+﻿using NSubstitute.ExceptionExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDotNet.ArgumentValidation.UnitTests;
+
+public sealed class EnumerableExtensions_NotEmpty_Array {
+
+	[Fact]
+	public void ReturnsCorrectly() {
+
+		ArgInfo<Array> argInfo = new( new int[] { 1 }, null, null );
+
+		ArgInfo<Array> result = EnumerableExtensions.NotEmpty( argInfo );
+
+		ArgInfoAssertions.Equal( argInfo, result );
+	}
+
+	[Fact]
+	public void WithSpecificArrayReturnsCorrectly() {
+
+		ArgInfo<int[]> argInfo = new( new int[] { 1 }, null, null );
+
+		ArgInfo<int[]> result = EnumerableExtensions.NotEmpty( argInfo );
+
+		ArgInfoAssertions.Equal( argInfo, result );
+	}
+
+	[Fact]
+	public void WithSpecificNullableArrayReturnsCorrectly() {
+
+		ArgInfo<int?[]> argInfo = new( new int?[] { 1 }, null, null );
+
+		ArgInfo<int?[]> result = EnumerableExtensions.NotEmpty( argInfo );
+
+		ArgInfoAssertions.Equal( argInfo, result );
+	}
+
+	[Fact]
+	public void WithNullValueReturnsCorrectly() {
+
+		ArgInfo<Array?> argInfo = new( null, null, null );
+
+		ArgInfo<Array?> result = EnumerableExtensions.NotEmpty( argInfo );
+
+		ArgInfoAssertions.Equal( argInfo, result );
+	}
+
+	[Fact]
+	public void WithEmptyValueThrowsArgumentException() {
+
+		string name = "Name";
+		Array value = Array.Empty<int>();
+
+		ArgumentException exception = Assert.Throws<ArgumentException>( name, () => {
+			ArgInfo<Array> argInfo = new( value, name, null );
+			_ = EnumerableExtensions.NotEmpty( argInfo );
+		} );
+
+		string expectedMessage = "Value cannot be empty.";
+
+		Assert.StartsWith( expectedMessage, exception.Message );
+	}
+
+	[Fact]
+	public void WithInvalidValueAndMessageThrowsArgumentException() {
+
+		string name = "Name";
+		Array value = Array.Empty<int>();
+		string message = "Message";
+
+		ArgumentException exception = Assert.Throws<ArgumentException>( name, () => {
+			ArgInfo<Array> argInfo = new( value, name, message );
+			_ = EnumerableExtensions.NotEmpty( argInfo );
+		} );
+
+		Assert.StartsWith( message, exception.Message );
+	}
+}
