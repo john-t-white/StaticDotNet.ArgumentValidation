78c5b74350e8e2944951698d85ea8eae9ce4e6b0
John White <johnwhite@kaplantrucking.com>
2022-12-05 08:12:55 -0500

    Removed uneeded exception messages.

--- src/StaticDotNet.ArgumentValidation/Constants.cs
+++ src/StaticDotNet.ArgumentValidation/Constants.cs
@@ -16,8 +16,6 @@ internal class Constants {
 
 	internal const string VALUE_MUST_BE_EQUAL_TO = "Value must be equal to {0}.";
 
-	internal const string VALUE_MUST_BE_FALSE = "Value must be false.";
-
 	internal const string VALUE_MUST_BE_GREATER_THAN = "Value must be greater than {0}.";
 
 	internal const string VALUE_MUST_BE_GREATER_THAN_OR_EQUAL_TO = "Value must be greater than or equal to {0}.";
@@ -27,12 +25,6 @@ internal class Constants {
 	internal const string VALUE_MUST_BE_LESS_THAN_OR_EQUAL_TO = "Value must be less than or equal to {0}.";
 
 	internal const string VALUE_MUST_BE_NULL = "Value must be null.";
-
-	internal const string VALUE_MUST_BE_READABLE = "Value must be readable.";
-
-	internal const string VALUE_MUST_BE_TRUE = "Value must be true.";
-
-	internal const string VALUE_MUST_BE_WRITEABLE = "Value must be writable.";
 
 	internal const string VALUE_MUST_CONTAIN = "Value must contain {0}.";
 
@@ -45,6 +37,4 @@ internal class Constants {
 	internal const string VALUE_MUST_HAVE_LENGTH_EQUAL_TO = "Value must have a length equal to {0}.";
 
 	internal const string VALUE_MUST_MATCH_REGEX = "Value must match the regex {0}.";
-
-	internal const string VALUE_MUST_NOT_BE_EQUAL_TO = "Value must not be equal to {0}.";
 }
