93a69228ae1646ea187323a8257f22bf9e12b211
John White <johnwhite@kaplantrucking.com>
2022-12-16 15:10:12 -0500

    Added benchmark to see how fast converting to boolean is.

--- src/StaticDotNet.ArgumentValidation.Benchmarks/Program.cs
+++ src/StaticDotNet.ArgumentValidation.Benchmarks/Program.cs
@@ -3,4 +3,5 @@ using StaticDotNet.ArgumentValidation.Benchmarks;
 
 //_ = BenchmarkRunner.Run<Is_Between>();
 //_ = BenchmarkRunner.Run<IsNotNull_AssignableTo>();
-_ = BenchmarkRunner.Run<IsNotNullOrWhiteSpace>();
+//_ = BenchmarkRunner.Run<IsNotNullOrWhiteSpace>();
+_ = BenchmarkRunner.Run<IsNotNull_NotWhiteSpace_ToBoolean>();
