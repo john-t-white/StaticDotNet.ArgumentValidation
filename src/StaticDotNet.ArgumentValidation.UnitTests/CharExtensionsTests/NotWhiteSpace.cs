869013fa4184accff5c953cef2b4c2fd2629a95c
John White <johnwhite@kaplantrucking.com>
2022-12-17 18:02:00 -0500

    Added NotWhiteSpace for char.

---
+++ src/StaticDotNet.ArgumentValidation.UnitTests/CharExtensionsTests/NotWhiteSpace.cs
@@ -0,0 +1,45 @@
+﻿namespace StaticDotNet.ArgumentValidation.UnitTests.CharExtensionsTests;
+
+public sealed class NotWhiteSpace {
+
+	[Fact]
+	public void ReturnsCorrectly() {
+
+		ArgInfo<char> argInfo = new( 'a', null, null );
+
+		ArgInfo<char> result = argInfo.NotWhiteSpace();
+
+		ArgInfoAssertions.Equal( argInfo, result );
+	}
+
+	[Fact]
+	public void WithWhiteSpaceValueThrowsArgumentException() {
+
+		char argumentValue = ' ';
+		string name = "Name";
+
+		ArgumentException exception = Assert.Throws<ArgumentException>( name, () => {
+			ArgInfo<char> argInfo = new( argumentValue, name, null );
+			_ = argInfo.NotWhiteSpace();
+		} );
+
+		string expectedMessage = "Value cannot be white space.";
+
+		Assert.StartsWith( expectedMessage, exception.Message );
+	}
+
+	[Fact]
+	public void WithInvalidValueAndMessageThrowsArgumentException() {
+
+		char argumentValue = ' ';
+		string name = "Name";
+		string message = "Message";
+
+		ArgumentException exception = Assert.Throws<ArgumentException>( name, () => {
+			ArgInfo<char> argInfo = new( argumentValue, name, message );
+			_ = argInfo.NotWhiteSpace();
+		} );
+
+		Assert.StartsWith( message, exception.Message );
+	}
+}
---
+++ src/StaticDotNet.ArgumentValidation/CharExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticDotNet.ArgumentValidation;
+
+public static class CharExtensions {
+
+	public static ref readonly ArgInfo<char> NotWhiteSpace( in this ArgInfo<char> argInfo ) {
+
+		if( !char.IsWhiteSpace( argInfo.Value ) ) {
+			return ref argInfo;
+		}
+
+		string message = argInfo.Message ?? Constants.VALUE_CANNOT_BE_WHITE_SPACE;
+		throw new ArgumentException( message, argInfo.Name );
+	}
+}
