2858e6a653631b22b3844753b8c6f6e6e022035a
John White <johnwhite@kaplantrucking.com>
2022-12-19 16:50:57 -0500

    Removed NotWhiteSpace from span as you should use generally ReadOnlySpan when dealing with strings.

--- src/StaticDotNet.ArgumentValidation/SpanExtensions.cs
+++ src/StaticDotNet.ArgumentValidation/SpanExtensions.cs
@@ -8,23 +8,6 @@ namespace StaticDotNet.ArgumentValidation;
 public static class SpanExtensions {
 
 	/// <summary>
-	/// Ensures an argument is not white space, otherwise an <see cref="ArgumentException"/> is thrown.
-	/// </summary>
-	/// <param name="argInfo">The argument info.</param>
-	/// <returns>The <paramref name="argInfo"/>.</returns>
-	/// <exception cref="ArgumentException">Thrown when <paramref name="argInfo.Value"/> is white space.</exception>
-	public static ref readonly SpanArgInfo<char> NotWhiteSpace( in this SpanArgInfo<char> argInfo ) {
-
-		for( int i = 0; i < argInfo.Value.Length; i++ ) {
-			if( !char.IsWhiteSpace( argInfo.Value[ i ] ) ) {
-				return ref argInfo;
-			}
-		}
-
-		throw new ArgumentException( argInfo.Message ?? Constants.VALUE_CANNOT_BE_WHITE_SPACE, argInfo.Name );
-	}
-
-	/// <summary>
 	/// Ensures an argument is not empty, otherwise an <see cref="ArgumentException"/> is thrown.
 	/// </summary>
 	/// <param name="argInfo">The argument info.</param>
