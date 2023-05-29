807e830d11e78d86f8cc75d272f33d287bb67651
John White <johnwhite@kaplantrucking.com>
2023-05-29 19:48:37 -0400

    Added unit tests for string Upper/Lower

---
+++ src/StaticDotNet.ArgumentValidation.UnitTests/StringExtensionsTests/Lower.cs
@@ -0,0 +1,45 @@
+﻿namespace StaticDotNet.ArgumentValidation.UnitTests.StringExtensionsTests;
+
+public sealed class Lower {
+
+	[Fact]
+	public void ReturnsCorrectly() {
+
+		ArgInfo<string> argInfo = new( "a", null, null );
+
+		ArgInfo<string> result = argInfo.Lower();
+
+		ArgInfoAssertions.Equal( argInfo, result );
+	}
+
+	[Fact]
+	public void WithNotLowerValueThrowsArgumentException() {
+
+		string argumentValue = "A";
+		string name = "Name";
+
+		ArgumentException exception = Assert.Throws<ArgumentException>( name, () => {
+			ArgInfo<string> argInfo = new( argumentValue, name, null );
+			_ = argInfo.Lower();
+		} );
+
+		string expectedMessage = $"Value \"{argumentValue}\" must be lower case.";
+
+		Assert.StartsWith( expectedMessage, exception.Message );
+	}
+
+	[Fact]
+	public void WithInvalidValueAndMessageThrowsArgumentException() {
+
+		string argumentValue = "A";
+		string name = "Name";
+		string message = "Message";
+
+		ArgumentException exception = Assert.Throws<ArgumentException>( name, () => {
+			ArgInfo<string> argInfo = new( argumentValue, name, message );
+			_ = argInfo.Lower();
+		} );
+
+		Assert.StartsWith( message, exception.Message );
+	}
+}
---
+++ src/StaticDotNet.ArgumentValidation.UnitTests/StringExtensionsTests/Upper.cs
@@ -0,0 +1,45 @@
+﻿namespace StaticDotNet.ArgumentValidation.UnitTests.StringExtensionsTests;
+
+public sealed class Upper {
+
+	[Fact]
+	public void ReturnsCorrectly() {
+
+		ArgInfo<string> argInfo = new( "A", null, null );
+
+		ArgInfo<string> result = argInfo.Upper();
+
+		ArgInfoAssertions.Equal( argInfo, result );
+	}
+
+	[Fact]
+	public void WithNotLowerValueThrowsArgumentException() {
+
+		string argumentValue = "a";
+		string name = "Name";
+
+		ArgumentException exception = Assert.Throws<ArgumentException>( name, () => {
+			ArgInfo<string> argInfo = new( argumentValue, name, null );
+			_ = argInfo.Upper();
+		} );
+
+		string expectedMessage = $"Value \"{argumentValue}\" must be upper case.";
+
+		Assert.StartsWith( expectedMessage, exception.Message );
+	}
+
+	[Fact]
+	public void WithInvalidValueAndMessageThrowsArgumentException() {
+
+		string argumentValue = "a";
+		string name = "Name";
+		string message = "Message";
+
+		ArgumentException exception = Assert.Throws<ArgumentException>( name, () => {
+			ArgInfo<string> argInfo = new( argumentValue, name, message );
+			_ = argInfo.Upper();
+		} );
+
+		Assert.StartsWith( message, exception.Message );
+	}
+}
